be5e06debf2e78753c8b6eeea04eacc741cb6833
Leighzer <bwc_nothgiel@live.com>
2020-10-25 13:44:07 -0400

    v0.1.2;

--- Source/data.lua
+++ Source/data.lua
@@ -1,5 +1,3 @@
---require('__debugadapter__/debugadapter.lua')
-
 if not leighzermods then --generic mod variable to store information that may be needed later
     leighzermods = {}
 end
@@ -11,14 +9,14 @@ if not leighzermods.leighzernuclearsciencepack then
     leighzermods.leighzernuclearsciencepack.resultCount = 3    
     leighzermods.leighzernuclearsciencepack.subgroup = "science-pack"
     leighzermods.leighzernuclearsciencepack.order = "f[utility-science-pack]-a" -- order immediately after utility science pack
+    leighzermods.leighzernuclearsciencepack.sciencePackName = "nuclear-science-pack"
 
 end
 
 -- create science pack item
-local sciencePackName = "nuclear-science-pack"
 local sciencePackTool = {
     type = "tool",
-    name = sciencePackName,
+    name = leighzermods.leighzernuclearsciencepack.sciencePackName,
     localised_name = "Nuclear science pack",
     icon = "__leighzernuclearsciencepack__/graphics/icons/nuclear-science-pack.png",
     icon_size = 64,
@@ -46,12 +44,12 @@ local sciencePackIngredients = {
     {"steam-turbine",1},
     {"uranium-fuel-cell",1}
 }
-leighzermods.utils.createRecipe(sciencePackName,leighzermods.leighzernuclearsciencepack.energyRequired, false, "crafting",sciencePackIngredients,sciencePackName,leighzermods.leighzernuclearsciencepack.resultCount,leighzermods.leighzernuclearsciencepack.subgroup,leighzermods.leighzernuclearsciencepack.order,true)
+leighzermods.utils.createRecipe(leighzermods.leighzernuclearsciencepack.sciencePackName,leighzermods.leighzernuclearsciencepack.energyRequired, false, "crafting",sciencePackIngredients,leighzermods.leighzernuclearsciencepack.sciencePackName,leighzermods.leighzernuclearsciencepack.resultCount,leighzermods.leighzernuclearsciencepack.subgroup,leighzermods.leighzernuclearsciencepack.order,true)
 
 -- create science pack technology
 -- add kovarex processing to technology prereq
 local sciencePackTechIcons = {{icon="__leighzernuclearsciencepack__/graphics/technology/nuclear-science-pack.png",icon_size=128}}
-local sciencePackTechEffects = {{type="unlock-recipe",recipe=sciencePackName}}
+local sciencePackTechEffects = {{type="unlock-recipe",recipe=leighzermods.leighzernuclearsciencepack.sciencePackName}}
 local sciencePackTechUnit = {
     count = 1500,
       ingredients =
@@ -64,44 +62,4 @@ local sciencePackTechUnit = {
       time = 30
 }
 local sciencePackTechPrereqs = {"kovarex-enrichment-process"}
-leighzermods.utils.createTechnology(sciencePackName,sciencePackTechIcons,true,sciencePackTechEffects,sciencePackTechUnit,sciencePackTechPrereqs,"c-b","Nuclear science pack","Allows research of powerful uranium weaponry, and proper spaceflight.")
-
--- make atom bomb prereq the science pack - remove kovarex prereq - add science pack as unit to atom bomb
-local atomTech = data.raw.technology["atomic-bomb"]
-if atomTech and atomTech.unit and atomTech.unit.ingredients and atomTech.prerequisites then
-    leighzermods.utils.removePrerequisite("atomic-bomb","kovarex-enrichment-process")
-    table.insert(atomTech.unit.ingredients,{sciencePackName,1})
-    table.insert(atomTech.prerequisites,sciencePackName)
-end
-
--- add tech as prereq to uranium ammo - add science pack as unit to uranium ammo
-local uraniumAmmoTech = data.raw.technology["uranium-ammo"]
-if uraniumAmmoTech and uraniumAmmoTech.unit and uraniumAmmoTech.unit.ingredients and uraniumAmmoTech.prerequisites then
-    table.insert(uraniumAmmoTech.unit.ingredients,{sciencePackName,1})
-    table.insert(uraniumAmmoTech.prerequisites,sciencePackName)
-end
-
--- add tech as prereq to rocket - add science pack as unit to rocket tech
-local rocketSiloTech = data.raw.technology["rocket-silo"]
-if rocketSiloTech and rocketSiloTech.unit and rocketSiloTech.unit.ingredients and rocketSiloTech.prerequisites then
-    table.insert(rocketSiloTech.unit.ingredients,{sciencePackName,1})
-    table.insert(rocketSiloTech.prerequisites,sciencePackName)
-end
-
--- add science pack to space science pack unit
-local spaceSciencePackTech = data.raw.technology["space-science-pack"]
-if spaceSciencePackTech and spaceSciencePackTech.unit and spaceSciencePackTech.unit.ingredients then
-    table.insert(spaceSciencePackTech.unit.ingredients,{sciencePackName,1})
-end
-
--- add uranium science pack to all techs that have space science as part of their research
-for k,v in pairs(data.raw.technology) do
-    if v and v.unit and v.unit.ingredients then
-        for i,vv in ipairs(v.unit.ingredients) do
-            if vv[1] and vv[1] == "space-science-pack" then
-                table.insert(v.unit.ingredients,{sciencePackName,1})
-                break
-            end
-        end
-    end
-end
+leighzermods.utils.createTechnology(leighzermods.leighzernuclearsciencepack.sciencePackName,sciencePackTechIcons,true,sciencePackTechEffects,sciencePackTechUnit,sciencePackTechPrereqs,"c-b","Nuclear science pack","Allows research of powerful uranium weaponry and proper spaceflight.")
